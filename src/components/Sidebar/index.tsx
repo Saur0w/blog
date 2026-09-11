8449bd3d66aa392c3d25676a2c33ecf68822ca0b
saur0w <sthapliyal085@gmail.com>
2026-09-11 19:40:12 +0530

    feat: add Sidebar component, install Sass, and configure IDE project settings

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <nav>
+
+            </nav>
+        </aside>
+    )
+}
